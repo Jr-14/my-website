f1bcca6e55322034899389f72d3c4f6d8ebbe6b9
John Emerson Requizo <58794199+Jr-14@users.noreply.github.com>
2023-07-23 23:04:18 +1000

    [Feature] Create blog layout and blog post (#16)
    * Add routing for blog posts
    
    * Remove dynamic paths
    
    * Add complete static blog post
    
    * Update readme to complete blog layout and first blog post

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -6,3 +6,11 @@
 <Navigation />
 
 <h1>Blog Posts</h1>
+
+<h2>2023</h2>
+
+<p>
+	07/23 - <a href="/blog/creating-and-deploying-my-first-website-as-a-project">
+		Creating and deploying my first website as a project
+	</a>
+</p>
