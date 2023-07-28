3b7f55b8fe07c7d2445762225fa1b7ca4e244b3b
John Requizo <jereq1@student.monash.edu>
2023-07-28 21:29:07 +1000

    Add Projects path and my website as initial project

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '../../global.css';
+	import Navigation from '../Navigation.svelte';
+</script>
+
+<Navigation />
+
+<h1>Projects</h1>
+
+<h2>My Website</h2>
+<p>
+    Showcasing what I've learn about building my website.
+</p>
+
+<a target="_blank" href="https://github.com/Jr-14/my-website">My website source code on github.</a>
