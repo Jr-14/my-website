1ef1b7f8df773f395d04b4da39ad4b7cc7b2a42d
John Emerson Requizo <jr.jemerson14@gmail.com>
2023-09-29 11:20:48 +1000

    Run prettier and lint

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -12,8 +12,6 @@
 <h1>Projects</h1>
 
 <h2>My Website</h2>
-<p>
-    Showcasing what I've learn about building my website.
-</p>
+<p>Showcasing what I've learn about building my website.</p>
 
 <a target="_blank" href="https://github.com/Jr-14/my-website">My website source code on github.</a>
